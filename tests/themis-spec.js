ebb51d38315276c2272e8c0beea70e0cb3823c11
airyland <airyland@qq.com>
2014-07-08 20:34:44 +0800

    add test

--- tests/themis-spec.js
+++ tests/themis-spec.js
@@ -1,10 +1,14 @@
 var expect = require('expect.js');
-var themis = require('../index');
+var Themis = require('../index');
 
-describe('themis', function() {
-
-  it('normal usage', function() {
-
-  });
+describe('themis', function () {
+    
+    it('normal usage', function () {
+        Themis.set('a', 'bb');
+        expect(Themis.get('a')).to.be('bb');
+        expect(localStorage.getItem('a')).to.be('"bb"');
+        expect(sessionStorage.getItem('a')).to.be('bb');
+        expect(/a=bb/.test(document.cookie)).to.be(true);
+    });
 
 });
